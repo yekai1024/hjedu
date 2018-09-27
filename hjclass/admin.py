47d608e1cff56a746a6504c6909fdb688de183a1
Kai Ye <yekai@cloudred.cn>
2018-09-28 00:55:33 +0800

    added action

--- hjclass/admin.py
+++ hjclass/admin.py
@@ -4,5 +4,12 @@ from .models import HJClass
 # Register your models here.
 class HJclassAdmin(admin.ModelAdmin):
     list_display = ['class_name', 'short_url', 'long_url', 'in_use']
+    actions = ['generate_html']
+
+    def generate_html(self, request, queryset):
+        for obj in queryset:
+            print(obj.class_name)
+        self.message_user(request, "Successfully generated class.")
+    generate_html.short_description = "Generate Class"
 
 admin.site.register(HJClass, HJclassAdmin)
